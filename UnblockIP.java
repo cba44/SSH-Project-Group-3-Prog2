5ac83a6aed25abf5362d105db0071e11ff969e90
Chiran Agalawatte <cba_44@yahoo.com>
2014-11-22 07:26:49 +0530

    Daemonizing and adding to a package

--- UnblockIP.java
+++ UnblockIP.java
@@ -1,29 +1,61 @@
 //Program 2
 
+package ssh2;
+
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.daemon.*;
 
-public class UnblockIP {
+public class UnblockIP implements Daemon{
     
-    public static void main(String [] args){
+    public static void main(String [] args){       
     
-    DBConnect1 connect = new DBConnect1();
-    
-    while (true){
-    
-        connect.releaseIP();
-        
-        try{
+        DBConnect1 connect = new DBConnect1();
 
-		TimeUnit.SECONDS.sleep(3);
+        while (true){
 
-	}catch(Exception ex){
+            try{
 
-	System.out.println(ex);
+                connect.releaseIP();
 
-	}
+                try{
+
+                        TimeUnit.SECONDS.sleep(3);
+
+                }catch(Exception ex){
+
+                System.out.println(ex);
+
+                }
+
+            }catch (Exception ex){
+
+                System.out.println(ex);
+
+            }
+
+        }
     
     }
     
+    @Override
+    public void init(DaemonContext dc) throws DaemonInitException, Exception {
+        System.out.println("initializing ...");
+    }
+    
+    @Override
+    public void start() throws Exception {
+        System.out.println("starting ...");
+        main(null);     //Running the main method
+    }
+
+    @Override
+    public void stop() throws Exception {
+        System.out.println("stopping ...");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("done.");
     }
     
 }
